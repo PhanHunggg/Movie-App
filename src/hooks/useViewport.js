778bfa144b86b9c7c2b02a20f1f34836ef7444bb
ThanhDat <caothanhdat02003@gmail.com>
2023-02-26 13:46:34 +0700

    fix css movieList

--- src/hooks/useViewport.js
+++ src/hooks/useViewport.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
 import { DESKTOP, LAPTOP, MOBILE, TABLET } from "../constants";
-=======
-import { DESKTOP, MOBILE, TABLET } from "../constants";
->>>>>>> 7d8eef60025bd765c3f1d0f8ed26d8bd5216b213
 
 export const useViewport = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -20,13 +16,7 @@ export const useViewport = () => {
 
   if (width <= 767) {
     return MOBILE;
-<<<<<<< HEAD
   } else if (width > 767 && width <= 960) {
-=======
-  } else if (width > 1020) {
-    return DESKTOP;
-  } else if (width > 767 && width <= 1024) {
->>>>>>> 7d8eef60025bd765c3f1d0f8ed26d8bd5216b213
     return TABLET;
   } else if (width > 960 && width <= 1200) {
     return LAPTOP;
